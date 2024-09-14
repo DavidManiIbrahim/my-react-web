f55093fec4d2a10b14c942832e173a38a292f059
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-09-14 14:55:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// import { useState } from 'react'
+// import LionImage from './assets/pexels-ardit-mbrati-LION.png'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <header>
+      <h1>ZOO</h1>
+      <nav>
+        <ul>
+          <li><a href="">HOME</a></li>
+          <li><a href="">GALLARY</a></li>
+          <li><a href="">ABOUT</a></li>
+          <li><a href="">CONTACT</a></li>
+        </ul>
+      </nav>
+      </header>
+
+      {/* <main>
+        <div className="image-container">
+          <img src='' alt="" />
+        </div>
+        <div className="text-container"></div>
+      </main> */}
+      <footer>
+        {/* <center> */}
+          2024&copy;
+
+        {/* </center> */}
+      </footer>
+    </>
+  )
+}
+
+export default App
