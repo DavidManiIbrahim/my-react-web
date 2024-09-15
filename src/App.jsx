a3013fd70099e2cf973323a9beb3b547a28c1c46
DavidManiIbrahim <ibrahimmanidavid@gmail.com>
2024-09-15 06:01:55 +0100

    added container,images,header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
-// import { useState } from 'react'
-// import LionImage from './assets/pexels-ardit-mbrati-LION.png'
-// import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './Header.jsx'
+import Container from './Container.jsx'
+import Footer from './Footer.jsx'
 
 function App() {
   
 
   return (
     <>
-      <header>
-      <h1>ZOO</h1>
-      <nav>
-        <ul>
-          <li><a href="">HOME</a></li>
-          <li><a href="">GALLARY</a></li>
-          <li><a href="">ABOUT</a></li>
-          <li><a href="">CONTACT</a></li>
-        </ul>
-      </nav>
-      </header>
-
-      {/* <main>
-        <div className="image-container">
-          <img src='' alt="" />
-        </div>
-        <div className="text-container"></div>
-      </main> */}
-      <footer>
-        {/* <center> */}
-          2024&copy;
-
-        {/* </center> */}
-      </footer>
+    <Header/>
+    <Container/>
+    <Footer/>
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+function Footer() {
+
+return(
+  <footer>
+    <center>
+      {new Date().getFullYear()}&copy;
+    </center>
+  </footer>
+);
+
+}
+
+export default Footer
